b8f8b0dae923e50e88445f1d1e5eddb0ecf60366
Yash Garg <80143921+Yashh-Garg@users.noreply.github.com>
2025-04-10 02:03:09 +0530

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 5173,
-    host: true
+    host: true,
+    allowedHosts: ['msn-clone-frontend.onrender.com'] //for Render.com
   }
 })
